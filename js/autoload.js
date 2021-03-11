d84ef25430f4fc48c131f298ca2dbf6398bab979
Zero <cnzeropro@qq.com>
2021-03-11 19:22:56 +0800

    live2d

--- js/autoload.js
+++ js/autoload.js
@@ -3,46 +3,47 @@ const live2d_path = "https://cdn.jsdelivr.net/gh/cnzeropro/live2d-widget/";
 
 // 封装异步加载资源方法
 function loadResourceAsync(url, type) {
-	return new Promise((resolve, reject) => {
-		let tag;
+  return new Promise((resolve, reject) => {
+    let tag;
 
-		if (type === "css") {
-			tag = document.createElement("link");
-			tag.rel = "stylesheet";
-			tag.href = url;
-		} else if (type === "js") {
-			tag = document.createElement("script");
-			tag.src = url;
-		} else if(type === "icon") {
-			tag = document.createElement("link");
-			tag.rel = "icon";
-			tag.href = url;
-		}
+    if (type === "css") {
+      tag = document.createElement("link");
+      tag.rel = "stylesheet";
+      tag.href = url;
+    } else if (type === "js") {
+      tag = document.createElement("script");
+      tag.src = url;
+    }
+    // else if(type === "icon") {
+    // 	tag = document.createElement("link");
+    // 	tag.rel = "icon";
+    // 	tag.href = url;
+    // }
 
-		if (tag) {
-			tag.onload = () => resolve(url);
-			tag.onerror = () => reject(url);
-			document.head.appendChild(tag);
-		}
-	});
+    if (tag) {
+      tag.onload = () => resolve(url);
+      tag.onerror = () => reject(url);
+      document.head.appendChild(tag);
+    }
+  });
 }
 
 // 加载所需资源
 if (screen.width >= 567) {
-	Promise.all([
-		loadResourceAsync(live2d_path + "css/waifu.css", "css"),
-		loadResourceAsync(live2d_path + "js/live2d.min.js", "js"),
-		loadResourceAsync(live2d_path + "js/waifu-tips.js", "js"),
-		loadResourceAsync(live2d_path + "favicon/favicon.ico", "icon")
-	]).then(() => {
-		initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
-	});
+  Promise.all([
+    // loadResourceAsync(live2d_path + "favicon/favicon.ico", "icon")
+    loadResourceAsync(live2d_path + "css/waifu.css", "css"),
+    // loadResourceAsync(live2d_path + "css/fontawesome.min.css", "css"),
+    loadResourceAsync(live2d_path + "js/live2dcubismcore.min.js", "js"),
+    loadResourceAsync(live2d_path + "js/waifu-tips.js", "js"),
+  ]).then(() => {
+    initWidget(live2d_path + "json/waifu-tips.json", "https://live2d.fghrsh.net/api");
+  });
 }
-// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
 // API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
 
 console.log(
-	`
+    `
   く__,.ヘヽ.        /  ,ー､ 〉
            ＼ ', !-─‐-i  /  /´
            ／｀ｰ'       L/／｀ヽ､
